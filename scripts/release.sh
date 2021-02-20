49b3cca623448b65a300f31287fbc75240028f19
Alex Sloan <alex.b.sloan@gmail.com>
2021-02-20 17:26:44 -0500

    Optimize caching of assets, add cache busting

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,5 +8,7 @@ JEKYLL_ENV=production bundle exec jekyll build
 
 gsutil -m rsync -r -d "${DIR}/../_site/" "gs://syschiff.art"
 
-# gsutil setmeta -h "Cache-Control: public, max-age=300" gs://my-quick-page/*.html
-# gsutil setmeta -h "Cache-Control: public, max-age=31536000" gs://my-quick-page/resources*/**
+# Default: cache for 5 minutes 
+gsutil -m setmeta -r -h "Cache-Control: public, max-age=300" "gs://syschiff.art/*"
+# Cache the assets directory for 30 days
+gsutil -m setmeta -r -h "Cache-Control: public, max-age=2592000" "gs://syschiff.art/assets/*"
